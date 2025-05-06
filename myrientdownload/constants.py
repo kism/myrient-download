ed3bd0ed39c1712f0d31f5a506bb0b4a748253bb
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-05-06 12:03:53 +0800

    cleanup

--- myrientdownload/constants.py
+++ myrientdownload/constants.py
@@ -4,6 +4,6 @@ HTTP_HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"  # noqa: E501 # This line is just long
 }
 
-REQUESTS_TIMEOUT = 20 # Yep, it can be slow
+REQUESTS_TIMEOUT = 20  # Yep, it can be slow
 
 FUN_TQDM_LOADING_BAR = " ▖▘▝▗▚▞█"
