e5a57e6f4bb6ad24c884fee756a567d744439496
Kieran Gee <kieran.lost.the.game@gmail.com>
2025-05-19 19:13:43 +0800

    fix pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,3 @@
-from myrientdownload.__main__ import print_program_info
-
-
-def test_print_program_info(caplog):
-    """Test the print_program_info function."""
-
-    from myrientdownload import __version__
-
-    with caplog.at_level("INFO"):
-        print_program_info()
-
-    assert __version__ in caplog.text
-
-
 def test_main_no_download(caplog, tmp_path, place_test_config, no_argparse, no_sleep):
     """Test the main function without downloading files."""
     from myrientdownload.__main__ import main
