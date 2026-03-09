f72f9685167e51494c627425fe89cf38f6442d52
Kieran Gee <kieran.lost.the.game@gmail.com>
2026-03-09 12:34:52 +0800

    fx

--- myrientdownload/__init__.py
+++ myrientdownload/__init__.py
@@ -1,6 +1,6 @@
 """Myrienent Download."""
 
-__version__ = "1.0.13"
+__version__ = "1.0.14"
 PROGRAM_NAME = "myrient-download"
 URL = "https://github.com/kism/myrient-download"
 DESCRIPTION = "A program to download files Myrient"
